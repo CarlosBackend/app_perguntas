d9705bee263125b8261a217782abdd83a3db1eea
Carlos Henrique <carlinhosbackend@gmail.com>
2025-10-30 12:42:49 -0300

    feat: criando Widget de questão

---
+++ lib/questao.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Questao extends StatelessWidget {
+  final String texto;
+  Questao(this.texto);
+  @override
+  Widget build(BuildContext context) {
+    return Text(texto);
+  }
+}
